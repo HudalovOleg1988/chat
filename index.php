7d78a716a271a055089d07dc550e6334c47e35e5
HudalovOleg1988 <55542403+HudalovOleg1988@users.noreply.github.com>
2024-10-16 16:57:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-
+<?php
+	// перевести все запросы в try catch
+	session_start();
+	if(isset($_SESSION['login']) && $_SESSION['login'])
+		header("Location: /chats/");
+	else
+		header("Location: /auth/");
+?>
